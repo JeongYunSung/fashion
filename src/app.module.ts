1a215ae4ef919766f3439e80c688c184944ba89d
JeongYunSung <123dbstjd@naver.com>
2022-12-08 22:34:19 +0900

    style: lint적용

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,13 +1,25 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { MysqlModule } from 'nest-mysql2';
 import { UserModule } from './user/user.module';
 import { CategoryModule } from './category/category.module';
 import { ProductModule } from './product/product.module';
 import { ImageModule } from './image/image.module';
 
 @Module({
-  imports: [UserModule, CategoryModule, ProductModule, ImageModule],
+  imports: [
+    MysqlModule.forRoot({
+      host: 'localhost',
+      user: 'root',
+      password: 'test',
+      database: 'testdb',
+    }),
+    UserModule,
+    CategoryModule,
+    ProductModule,
+    ImageModule,
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
