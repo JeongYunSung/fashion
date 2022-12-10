1473ddf350f3172fa3827ff7d31e946472b5b84a
JeongYunSung <123dbstjd@naver.com>
2022-12-10 12:07:18 +0900

    feature: 유저 조회 기능 및 주석 추가

--- src/user/user.controller.ts
+++ src/user/user.controller.ts
@@ -1,4 +1,5 @@
-import { Body, Controller, Post } from '@nestjs/common';
+import { Body, Controller, Get, Post, Req, UseGuards } from '@nestjs/common';
+import { JwtAuthGuard } from 'src/auth/jwt-auth.guard';
 import { User } from './user.mapper';
 import { UserService } from './user.service';
 
@@ -6,8 +7,27 @@ import { UserService } from './user.service';
 export class UserController {
   constructor(private readonly userService: UserService) {}
 
+  /**
+   * @Description - 유저를 생성하는 API
+   * @param user - { "email": "email", "password": "password", "name": "name", "role": "role" }형식에 맞게 유저정보를 받는다.
+   */
   @Post()
   async createUser(@Body() user: User) {
     return this.userService.createUser(user);
+  }
+
+  /**
+   * @description - 유저 정보를 조회하는 API
+   * @param req - 마찬가지로 Bearer Token을 통해 유저 정보를 가져온다.
+   * @returns - { "id": id, "email": "email", "password": "password", "name": "name", "role": "role" }형식에 맞게 유저정보를 반환한다.
+   */
+  @Get()
+  @UseGuards(JwtAuthGuard)
+  async getUser(@Req() req) {
+    const user = this.userService.getUser(req.user.email);
+    if (user) {
+      return user;
+    }
+    return { message: 'User not found' };
   }
 }
