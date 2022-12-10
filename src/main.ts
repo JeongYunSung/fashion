2c1d06a60efae5e215ccf523922e8fe6e799b07a
JeongYunSung <123dbstjd@naver.com>
2022-12-10 20:16:18 +0900

    chore: fnm 및 cors정책 설정

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,15 @@
 import { NestFactory } from '@nestjs/core';
+import { json } from 'express';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  app.use(json({ limit: '5mb' }));
+  app.enableCors({
+    origin: '*',
+    methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
+    allowedHeaders: 'Content-Type, Authorization, Accept',
+  });
   await app.listen(3000);
 }
 bootstrap();
